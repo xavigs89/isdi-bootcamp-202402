a0a562ccfa2cca83f0534ba62f2d6eb59b7395ce
xavigs89 <xavigs1989@gmail.com>
2024-02-16 11:21:20 +0100

    update array find #200

--- staff/xavi-gonzalez/arrays/find.js
+++ staff/xavi-gonzalez/arrays/find.js
@@ -1,14 +1,15 @@
 // El método find() devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
 
-delete Array.prototype.from;
+delete Array.prototype.find;
 
 debugger;
 function find(array, element) {
   var newArray = [];
   debugger;
-  for (var i = 0; i < array.length; i++)
-    if (array[i] === element) 
-    newArray[newArray.length] = array[i];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === element) newArray[newArray.length] = array[i];
+    return newArray
+}
 }
 
 //CASE 1
@@ -17,4 +18,4 @@ const array1 = [5, 12, 8, 130, 44];
 const found = array1.find((element) => element > 20);
 
 console.log(found);
-// Expected output: 12
+// Expected output: 130
