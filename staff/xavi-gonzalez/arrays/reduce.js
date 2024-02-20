6e62695e03707bd3d59316d1f31915fe9b86b7c7
xavigs89 <xavigs1989@gmail.com>
2024-02-20 18:43:22 +0100

    update arrays #200

--- staff/xavi-gonzalez/arrays/reduce.js
+++ staff/xavi-gonzalez/arrays/reduce.js
@@ -1,3 +1,4 @@
+//El método reduce() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
 
 delete Array.prototype.reduce
 
--- staff/xavi-gonzalez/arrays/splice.js
+++ staff/xavi-gonzalez/arrays/splice.js
@@ -1,6 +1,12 @@
+//El método splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
 
 delete Array.prototype.splice
 
 function splice() {
+
+    
     //TODO implement me
 }
+
+
+
