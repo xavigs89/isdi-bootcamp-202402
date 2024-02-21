c533754b6096c82a24c04bac797aa36ad9c66ec9
xavigs89 <xavigs1989@gmail.com>
2024-02-21 19:31:40 +0100

    update objects #346

--- staff/xavi-gonzalez/objects/add.js
+++ staff/xavi-gonzalez/objects/add.js
@@ -7,12 +7,24 @@
  * @throws {TypeError} When object is not an object.
  */
 function add(object, value) {
-    object[object.length] = value
+    if (arguments.length === 1) {
+        return object.length
+    }
+
+    else if (!(object instanceof Object)) {
+        throw new TypeError(object + ' is not an Object') 
+        }
+
+    else { 
+    object[object.length] = value 
 
     object.length++
 
     return object.length
+    }
 }
+
+
 
 console.log('CASE 1: add violet in colors')
 
@@ -38,3 +50,59 @@ console.log(colors)
     length: 4
 }
 */
+
+
+console.log('CASE 2: what if you do not assign a value');
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+var length = add(colors)
+
+console.log(length)
+// 4
+
+console.log(colors)
+/*
+{
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+*/
+
+
+console.log('CASE 3: fails on undefined as object parameter')
+
+var string = "pepe"
+try {
+    result = add(string, 85)
+} catch (error) {
+    console.log(error)
+    // TypeError: pepe is not an object
+}
+
+
+console.log('CASE 4: fails on undefined as object parameter')
+
+try {
+    result = add(undefined, string)
+} catch (error) {
+    console.log(error)
+    // TypeError: undefined is not an object
+}
+
+
+console.log('CASE 5: fails on undefined as object parameter')
+
+try {
+    result = add(68, string)
+} catch (error) {
+    console.log(error)
+    // TypeError: 68 is not an object
+}
--- staff/xavi-gonzalez/objects/extract.js
+++ staff/xavi-gonzalez/objects/extract.js
@@ -1,34 +1,50 @@
 /**
  * Extracts an element that matches the condition from an iterable object.
  *
- * @param object - The iterable object to mutate. 
+ * @param object - The iterable object to mutate.
  * @param index - The index from which to extract a value.
- * 
+ *
  * @throws {TypeError} When object is not an object, or when index is not a number.
  */
 function extract(object, callback) {
-    // TODO
-}
+  //   if (object instanceof Object) {
+  // TODO implement me
+  var extractedItem = {};
 
-console.log('CASE 1: extract user pepito form users')
+  for (var i = 0; i < object.length; i++) {
+    if (callback(object[i]) === true) {
+      extractedItem = object[i];
+      /*for (var j = i; j < object.length; j++) {
+     object[j] = object[j + 1];
+        }*/
+
+      object.length--;
+      delete object[object.length];
+    }
+  }
+  return extractedItem;
+}
+//   delete object[object.length - 1];
+
+console.log("CASE 1: extract user pepito form users");
 
 var users = {
-    0: { name: 'Wendy', age: 19 },
-    1: { name: 'Peter', age: 20 },
-    2: { name: 'Pepito', age: 50 },
-    3: { name: 'Campa', age: 30 },
-    4: { name: 'James', age: 40 },
-    length: 5
-}
+  0: { name: "Wendy", age: 19 },
+  1: { name: "Peter", age: 20 },
+  2: { name: "Pepito", age: 50 },
+  3: { name: "Campa", age: 30 },
+  4: { name: "James", age: 40 },
+  length: 5,
+};
 
 var user = extract(users, function (user) {
-    return user.name === 'Pepito'
-})
+  return user.name === "Pepito";
+});
 
-console.log(user)
+console.log(user);
 // { name: 'Pepito', age: 50 }
 
-console.log(users)
+console.log(users);
 /*
 {
     0: { name: 'Wendy', age: 19 },
