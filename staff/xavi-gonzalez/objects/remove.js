83316ae29c266d91251999f2593da1228c05332c
xavigs89 <xavigs1989@gmail.com>
2024-02-22 19:38:45 +0100

    update objects #346

--- staff/xavi-gonzalez/objects/remove.js
+++ staff/xavi-gonzalez/objects/remove.js
@@ -1,38 +1,57 @@
+/*if (!(object instanceof Object))
+      throw new TypeError(object + " is not an Object");
+*/
 /**
  * Removes an element in iterable object at specfified index.
  *
- * @param object - The iterable object to mutate. 
+ * @param object - The iterable object to mutate.
  * @param index - The index from which to remove a value.
- * 
+ *
  * @throws {TypeError} When object is not an object, or when index is not a number.
  */
 function remove(object, index) {
-    // TODO
-    var removedItem = {}
-    for (var i = index; i < object.length; i++)
-    
+  // TODO
 
-    if (!(object instanceof Object)) throw new TypeError(object + ' is not an Object')
+  if (!(object instanceof Object)) {
+    throw new TypeError(object + " is not an Object");
+  }
 
+  var removedItem = object[index];
 
-    return removedItem
+  for (var i = index; i < object.length; i++) {
+    object[i] = object[i + 1];
+  }
+  object.length--;
+  delete object[object.length];
+
+  /*for (var i = object.length -1; i > index; i--) {
+    object [i - index] = object [i]  
 }
 
-console.log('CASE 1: remove blue from index 1')
+
+object.length --
+
+delete object [object.length]
+*/
+
+  return removedItem;
+}
+
+console.log("CASE 1: remove blue from index 1");
 
 var colors = {
-    0: 'red',
-    1: 'blue',
-    2: 'green',
-    length: 3
-}
+  0: "red",
+  1: "blue",
+  2: "green",
+  length: 3,
+};
 
-var removed = remove(colors, 1)
+var removed = remove(colors, 1);
 
-console.log(removed)
+console.log(removed);
 // 'blue'
 
-console.log(colors)
+console.log(colors);
 /*
 {
     0: 'red',
@@ -41,21 +60,21 @@ console.log(colors)
 }
 */
 
-console.log('CASE 2: remove red from index 0')
+console.log("CASE 2: remove red from index 0");
 
 var colors = {
-    0: 'red',
-    1: 'blue',
-    2: 'green',
-    length: 3
-}
+  0: "red",
+  1: "blue",
+  2: "green",
+  length: 3,
+};
 
-var length = remove(colors, 0)
+var length = remove(colors, 0);
 
-console.log(length)
+console.log(length);
 // 'red'
 
-console.log(colors)
+console.log(colors);
 /*
 {
     0: 'blue',
@@ -64,36 +83,36 @@ console.log(colors)
 }
 */
 
-console.log('CASE 3: fails on undefind object parameter')
+console.log("CASE 3: fails on undefined object parameter");
 
 try {
-    remove()
+  remove();
 } catch (error) {
-    console.log(error)
-    // TypeError: undefined is not an Object
+  console.log(error);
+  // TypeError: undefined is not an Object
 }
 
-console.log('CASE 4: fails on 1 as an object parameter')
+console.log("CASE 4: fails on 1 as an object parameter");
 
 try {
-    remove(1)
+  remove(1);
 } catch (error) {
-    console.log(error)
-    // TypeError: 1 is not an Object
+  console.log(error);
+  // TypeError: 1 is not an Object
 }
 
-console.log('CASE 5: fails on undefined as index parameter')
+console.log("CASE 5: fails on undefined as index parameter");
 
 var colors = {
-    0: 'red',
-    1: 'blue',
-    2: 'green',
-    length: 3
-}
+  0: "red",
+  1: "blue",
+  2: "green",
+  length: 3,
+};
 
 try {
-    remove(colors)
+  remove(colors);
 } catch (error) {
-    console.log(error)
-    // TypeError: undefined is not a Number
+  console.log(error);
+  // TypeError: undefined is not a Number
 }
