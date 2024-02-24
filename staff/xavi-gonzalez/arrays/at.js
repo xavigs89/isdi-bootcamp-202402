aa46398b8bade1479faa6302610747dad03249c0
xavigs89 <xavigs1989@gmail.com>
2024-02-24 14:26:01 +0100

    update asserts in at function #200

--- staff/xavi-gonzalez/arrays/at.js
+++ staff/xavi-gonzalez/arrays/at.js
@@ -19,18 +19,22 @@ function at(array, index) {
   return result;
 }
 
+
+
 //CASE 1
 var nums = [100, 200, 300, 400, 500];
 var num = at(nums, 3);
 
 
-console.log(num);
+//console.log(num);
 //OUTPUT 400
 
 
+
+//ASSERTS
+
 console.assert(num === 400, '400')
 //400
-
 
 console.assert(nums[0] === 100, '100')
 console.assert(nums[1] === 200, '200')
@@ -41,6 +45,9 @@ console.assert(nums[4] === 500, '500')
 console.assert(nums.length === 5, '5')
 // [5]
 
+
+
+//FUNCIONES PARA ASSERTS
 var expectedResult = [100, 200, 300, 400, 500]
 
 for (let i = 0; i < nums.length; i++) {
@@ -50,29 +57,132 @@ console.assert(nums.length === expectedResult.length, expectedResult.length)
 
 
 
-/*
 
 //CASE 2
 var chars = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"];
 var char = at(chars, 4);
 
-console.log(char);
+//console.log(char);
 //OUTPUT " "
+
+
+//ASSERTS
+
+console.assert(char === ' ', ' ')
+//' '
+
+console.assert(chars[0] === 'h', 'h')
+console.assert(chars[1] === 'o', 'o')
+console.assert(chars[2] === 'l', 'l')
+console.assert(chars[3] === 'a', 'a')
+console.assert(chars[4] === ' ', ' ')
+console.assert(chars[5] === 'm', 'm')
+console.assert(chars[6] === 'u', 'u')
+console.assert(chars[7] === 'n', 'n')
+console.assert(chars[8] === 'd', 'd')
+console.assert(chars[9] === 'o', 'o')
+
+// ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+console.assert(chars.length === 10, '10')
+// [10]
+
+
+//FUNCIONES PARA ASSERTS
+var expectedResult = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+
+for (let i = 0; i < chars.length; i++) {
+  console.assert(chars[i] === expectedResult[i], expectedResult[i])
+}
+console.assert(chars.length === expectedResult.length, expectedResult.length) 
+
+
+
+
 
 //CASE 3
 var chars = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"];
 var char = at(chars, -3);
 
-console.log(char);
+//console.log(char);
 //OUTPUT "n"
+
+//ASSERTS
+
+console.assert(char === 'n', 'n')
+//'n'
+
+console.assert(chars[0] === 'h', 'h')
+console.assert(chars[1] === 'o', 'o')
+console.assert(chars[2] === 'l', 'l')
+console.assert(chars[3] === 'a', 'a')
+console.assert(chars[4] === ' ', ' ')
+console.assert(chars[5] === 'm', 'm')
+console.assert(chars[6] === 'u', 'u')
+console.assert(chars[7] === 'n', 'n')
+console.assert(chars[8] === 'd', 'd')
+console.assert(chars[9] === 'o', 'o')
+
+// ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+console.assert(chars.length === 10, '10')
+// [10]
+
+
+//FUNCIONES PARA ASSERTS
+var expectedResult = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+
+for (let i = 0; i < chars.length; i++) {
+  console.assert(chars[i] === expectedResult[i], expectedResult[i])
+}
+console.assert(chars.length === expectedResult.length, expectedResult.length) 
+
+
+
+
 
 //CASE 4
 var chars = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"];
 var char = at(chars, -30);
 
-console.log(char);
+//console.log(char);
 //OUTPUT undefined
 
+
+//ASSERTS
+
+console.assert(char === undefined, 'undefined')
+//'n'
+
+console.assert(chars[0] === 'h', 'h')
+console.assert(chars[1] === 'o', 'o')
+console.assert(chars[2] === 'l', 'l')
+console.assert(chars[3] === 'a', 'a')
+console.assert(chars[4] === ' ', ' ')
+console.assert(chars[5] === 'm', 'm')
+console.assert(chars[6] === 'u', 'u')
+console.assert(chars[7] === 'n', 'n')
+console.assert(chars[8] === 'd', 'd')
+console.assert(chars[9] === 'o', 'o')
+
+// ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+console.assert(chars.length === 10, '10')
+// [10]
+
+
+//FUNCIONES PARA ASSERTS
+var expectedResult = ["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+
+for (let i = 0; i < chars.length; i++) {
+  console.assert(chars[i] === expectedResult[i], expectedResult[i])
+}
+console.assert(chars.length === expectedResult.length, expectedResult.length) 
+
+
+
+
+
+
+
+/*
 //SOLUCION ALTERNATIVA:
 function at(array, index) {
   if (index > -1) {
