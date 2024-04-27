95fac7c39defca0782ddeb0062645ee1eee3a377
xavigs89 <xavigs1989@gmail.com>
2024-04-27 13:51:28 +0200

    add styles in Login.jsx page in app/src/pages

--- staff/xavi-gonzalez/project/app/src/pages/Login.jsx
+++ staff/xavi-gonzalez/project/app/src/pages/Login.jsx
@@ -40,20 +40,33 @@ function Login({ onUserLoggedIn, onRegisterClick }) {
 
     logger.debug('Login -> render')
 
-    return <main>
-        <h1>Login</h1>
+    return <main className="flex justify-center items-center h-screen bg-[#1B1F47]">
+        {/* <h1>Login</h1> */}
 
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input id="email" />
+        <div>
 
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <div className="flex justify-center items-center">
+                <img className="w-[200px] absolute top-20" src="" alt="LOGO" />
+            </div>
 
-            <button className="round-button" type="submit">Login</button>
-        </form>
+            <form onSubmit={handleSubmit} className="flex flex-col items-center mt-8">
+                <div className="flex flex-col mb-4">
+                    <label htmlFor="email" className="text-white mb-1">Email</label>
+                    <input id="email" className="rounded-lg px-2 py-1" />
+                </div>
 
-        <a href="" onClick={handleRegisterClick}>Register</a>
+                <div className="flex flex-col mb-4">
+                    <label htmlFor="password" className="text-white mb-1">Password</label>
+                    <input type="password" id="password" className="rounded-lg px-2 py-1" />
+                </div>
+
+                <button className="bg-[#4C5D8B] hover:bg-[#6B99C3] text-white font-bold py-2 px-4 rounded mt-4" type="submit">Login</button>
+            </form>
+
+        </div>
+        <div class="fixed bottom-8 items-center">
+            <p class="font-bold text-white">Don't have an account?<a href="" onClick={handleRegisterClick} class="text-[#F7C815] mt-2"> Register</a></p>
+        </div>
 
     </main>
 }
